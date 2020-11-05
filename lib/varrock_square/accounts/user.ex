d5a82b9dbda6034c104c33a7096312cd97427aac
Brandon Zimmerman <bzimmermandev@icloud.com>
2020-11-05 08:14:02 -0600

    Re-organized the schema and migrations for users table

--- lib/varrock_square/accounts/user.ex
+++ lib/varrock_square/accounts/user.ex
@@ -7,47 +7,89 @@ defmodule VarrockSquare.Accounts.User do
   alias Ecto.Changeset
 
   schema "users" do
+    # Username can never be changed.
     field :username, :string
-    field :role, :string, default: "user"
+    # Email can be used for password/2FA reset and login.
     field :email, :string
-    field :rsn, :string
-    field :has_avatar, :boolean, default: false
-    field :bio, :string
+    # Password; only :password_hash is stored in the database.
     field :password, :string, virtual: true
     field :password_hash, :string
-    # TODO Potential for future 2-factor authentication (see: shorturl.at/mAHX2)
+    # Age isn't collected, we just confirm the user is age 13+ at account creation time.
+    field :age, :integer, virtual: true
+    # The role describes user permissions across the site.
+    field :role, :string, default: "user"
+    # TODO Two-factor authentication feature (see: shorturl.at/mAHX2).
     field :has_2fa, :boolean, default: false
+    # Avatars are stored at a predictable public URL like [username].png so a flag is OK.
+    field :has_avatar, :boolean, default: false
+    # RSN is an optional OSRS in-game name.
+    field :rsn, :string
+    # User's bio, up to 4000 characters.
+    field :bio, :string
+    # User join & last-modified date.
     timestamps()
   end
 
-  def simple_changeset(user, params) do
+  @doc """
+  This changeset is used whenever a user needs to update their basic account information. It doesn't require any fields but accepts and validates `:rsn`, `:has_avatar` and `:bio` fields. `:rsn` must be unique.
+  """
+  def basic_info_changeset(user, params) do
     user
-    |> cast(params, [:username, :email, :rsn, :has_avatar, :bio])
-    |> validate_required([:username, :email])
-    |> unique_constraint([:username, :email, :rsn])
-    |> validate_length(:username, max: 20)
-    |> validate_length(:email, min: 6, max: 320)
+    |> cast(params, [:rsn, :has_avatar, :bio])
     |> validate_length(:rsn, max: 12)
-    |> validate_length(:bio, max: 4096)
+    |> validate_length(:bio, max: 4000)
+    |> unique_constraint([:rsn])
   end
 
-  def registration_changeset(user, params) do
+  @doc """
+  This changeset is used whenever a user needs to update their email & password atomically. It requires that the `:email` and `:password` fields be set and valid, as well as requiring a matching `:password` confirmation field.
+  """
+  def auth_changeset(user, params) do
     user
-    |> simple_changeset(params)
-    |> cast(params, [:password, :has_2fa])
-    |> validate_required([:password])
-    # NOTE Won't be required for a login flow, so perhaps do this conditionally
-    # |> validate_confirmation(:password, required: true, message: "password fields do not match")
+    |> cast(params, [:email, :password])
+    |> validate_required([:email, :password])
+    |> validate_length(:email, min: 6, max: 320)
+    |> unique_constraint([:email])
     |> validate_length(:password, min: 6, max: 100)
+    |> validate_confirmation(:password, required: true, message: "password fields do not match")
     |> hash_password()
   end
 
+  @doc """
+  This changeset should be used **once** when a user account is being registered. It casts and validates all of the same fields that `basic_info_changeset` and `auth_changeset` do, in addition to casting and validating the `:age` and `:username` fields. `:username` is created at registration and should never be changed.
+  """
+  def registration_changeset(user, params) do
+    user
+    |> basic_info_changeset(params)
+    |> auth_changeset(params)
+    |> cast(params, [:username, :age])
+    |> validate_required([:username, :age])
+    |> validate_length(:username, max: 20)
+    |> unique_constraint([:username])
+    |> validate_inclusion(:age, 13..126)
+  end
+
+  @doc """
+  This changeset should be used whenever a user needs to update their 2FA settings. It only requires that the `:has_2fa` field be set to `true` or `false`.
+  """
+  def two_factor_auth_changeset(user, params) do
+    user
+    |> cast(params, [:has_2fa])
+    |> validate_required([:has_2fa])
+  end
+
+  @doc """
+  This changeset should be used whenever an admin needs to update the role of some user. It only requires that the `:role` field be set to one of `:user`, `:premium_user`, `:mod` or `:admin`.
+  """
+  @roles ~w[user premium_user mod admin]
   def role_changeset(user, params) do
     user
     |> cast(params, [:role])
     |> validate_required([:role])
-    |> validate_inclusion(:role, ~w[user premium_user mod admin])
+    |> validate_inclusion(:role, @roles)
   end
+
+  #### CHANGESET PLUGINS ####
 
   defp hash_password(changeset) do
     case changeset do
--- priv/repo/migrations/20201105094632_create_users.exs
+++ priv/repo/migrations/20201105094632_create_users.exs
@@ -2,17 +2,16 @@ defmodule VarrockSquare.Repo.Migrations.CreateUsers do
   use Ecto.Migration
 
   def change do
-    # Create a table without the default user_id primary key; we'll use the username as the unique primary key
+    # Create a table without a default primary key; we'll use the username as a unique key.
     create table(:users, primary_key: false) do
       add :username, :string, null: false, size: 20, primary_key: true
-      # NOTE A role should be one of ~w[user premium_user mod admin]
-      add :role, :string, null: false, default: "user"
       add :email, :string, null: false, size: 320
-      add :rsn, :string, size: 12
-      add :has_avatar, :boolean, null: false, default: false
-      add :bio, :string, size: 4096
       add :password_hash, :string, null: false
+      add :role, :string, null: false, default: "user"
       add :has_2fa, :boolean, null: false, default: false
+      add :has_avatar, :boolean, null: false, default: false
+      add :rsn, :string, size: 12
+      add :bio, :string, size: 4096
       timestamps()
     end
 
