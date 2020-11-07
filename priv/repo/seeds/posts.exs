80c7b292fa04b8dbd14f23327890b9965d891d4a
Brandon Zimmerman <bzimmermandev@icloud.com>
2020-11-07 01:26:13 -0600

    More realistic seeds

--- priv/repo/seeds/posts.exs
+++ priv/repo/seeds/posts.exs
@@ -3,14 +3,18 @@ alias VarrockSquare.Accounts.User
 alias VarrockSquare.Content.Post
 
 post_params = [
-  {"TestDummy", %{slug: "bryo-guide-f5xp81", title: "Bryo Guide", body: "# Bryo Guide\n"}},
-  {"TrustNoBanks", %{slug: "welcome-varrock-square-nf02mq", title: "Welcome to Varrock Square!"}}
+  {"TrustNoBanks", %{slug: "welcome-varrock-square-nf02mq", title: "Welcome to Varrock Square!"}},
+  {"say_f2p_ult",
+   %{
+     slug: "bryo-guide-f5xp81",
+     title: "Bryo Guide",
+     body: "# Bryo Guide\n\n- Slap hard\n\nTo be continued…"
+   }}
 ]
 
 # Create a bunch of unpublished posts
-post_params
-|> Enum.map(fn {username, attrs} ->
+for {username, attrs} <- post_params do
   Repo.get!(User, username)
   |> Ecto.build_assoc(:posts, attrs)
   |> Repo.insert!()
-end)
+end
--- priv/repo/seeds/users.exs
+++ priv/repo/seeds/users.exs
@@ -1,30 +1,30 @@
 alias VarrockSquare.Repo
 alias VarrockSquare.Accounts.User
 
-user_params = [
+user_registration_params = [
   %{
     username: "TrustNoBanks",
     age: 27,
-    email: "bzimmermandev@icloud.com",
+    email: "admin@varrocksquare.net",
     password: "password123",
     password_confirmation: "password123"
   },
   %{
-    username: "TestDummy",
+    username: "UncleTomas",
     age: 13,
-    email: "test.dummy+varrocksquare@example.com",
+    email: "admin@f2p.wiki",
     password: "password456",
     password_confirmation: "password456"
   },
   %{
-    username: "TestDummyJr",
-    age: 27,
-    email: "jr-testdummy@gmail.com",
+    username: "say_f2p_ult",
+    age: 90,
+    email: "theotheradmin@f2p.wiki",
     password: "foobar",
     password_confirmation: "foobar"
   }
 ]
 
-user_params
+user_registration_params
 |> Enum.map(&User.registration_changeset(%User{}, &1))
 |> Enum.map(&Repo.insert!/1)
