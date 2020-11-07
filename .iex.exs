a77180360a5a6641a8410f203eaf83548b0bfdab
Brandon Zimmerman <bzimmermandev@icloud.com>
2020-11-07 01:02:54 -0600

    Added an .iex.exs file

---
+++ .iex.exs
@@ -0,0 +1,6 @@
+# NOTE This IEx config file will override your global user config options at ~/.iex.exs. If you have a global config in your home directory that you would like to apply as well, you can uncomment this line:
+# import_file("~/.iex.exs")
+
+alias VarrockSquare.Repo
+alias VarrockSquare.Accounts.User
+alias VarrockSquare.Content.Post
